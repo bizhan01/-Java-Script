c3cc8fefb85369a8e723591f0c9397e57d90d9bc
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-28 12:49:09 +0430

    my commit

---
+++ Interactive Image.js
@@ -0,0 +1,7 @@
+const images = ["image1.jpg", "image2.jpg", "image3.jpg"];
+let currentImageIndex = 0;
+
+function showNextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    document.getElementById("galleryImage").src = images[currentImageIndex];
+}
