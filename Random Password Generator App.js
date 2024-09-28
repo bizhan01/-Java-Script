243891f200e495648d8e21cdf074abe1ae33f2a7
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-28 12:02:48 +0430

    ssfsdfsdssw234234sdfssdfas

---
+++ Random Password Generator App.js
@@ -0,0 +1,10 @@
+function generateRandomPassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return password;
+}
+
+console.log(`Random Password: ${generateRandomPassword(8)}`);
