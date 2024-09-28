7ae0e6b4357994961ba6e1cc355be79acdcddcf9
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-28 12:02:15 +0430

    ssfsdfsdssw234234sdfs

---
+++ BMI Calculator App.js
@@ -0,0 +1,7 @@
+function calculateBMI(weight, height) {
+    return (weight / Math.pow(height, 2)).toFixed(2);
+}
+
+const weight = 70; // in kilograms
+const height = 1.75; // in meters
+console.log(`BMI: ${calculateBMI(weight, height)}`);
