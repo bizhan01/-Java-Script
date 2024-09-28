5d9df343b32fecdcadb73004fade4f8ffc845304
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-28 12:05:30 +0430

    ssfsdfsdssw234234sdfssdfasfsd

---
+++ Simple Currency Converter App.js
@@ -0,0 +1,7 @@
+function convertCurrency(amount, exchangeRate) {
+    return amount * exchangeRate;
+}
+
+const amountInUSD = 100;
+const exchangeRate = 0.85; // EUR to USD
+console.log(`Equivalent amount in EUR: ${convertCurrency(amountInUSD, exchangeRate)}`);
