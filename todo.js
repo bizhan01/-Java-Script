9c4e0a9a37c54249cbbd216d15bf923c37f63dc0
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-28 11:48:52 +0430

    ssfsdfsdss

---
+++ todo.js
@@ -0,0 +1,16 @@
+let todoList = [];
+
+function addTodo(item) {
+    todoList.push(item);
+}
+
+function displayTodos() {
+    console.log("Todos: ");
+    todoList.forEach((item, index) => {
+        console.log(`${index + 1}. ${item}`);
+    });
+}
+
+addTodo("Buy groceries");
+addTodo("Water the plants");
+displayTodos();
