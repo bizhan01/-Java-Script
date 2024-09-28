bdfa20a5c7092d1c8c72a9cc4b620cd82f5da093
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-28 13:00:14 +0430

    my commit

---
+++ Scissors Game.js
@@ -0,0 +1,6 @@
+const choices = ["rock", "paper", "scissors"];
+
+function playGame(userChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    // Determine the winner and display the result on the GUI
+}
