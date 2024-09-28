aef784fa7e9185028554a01d9e9925f70e85d73d
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-28 12:01:25 +0430

    ssfsdfsdssw234234sdfs

---
+++ Simple Countdown Timer App.js
@@ -0,0 +1,12 @@
+function countdown(seconds) {
+    let timer = setInterval(() => {
+        console.log(seconds);
+        seconds--;
+        if (seconds < 0) {
+            clearInterval(timer);
+            console.log("Countdown complete!");
+        }
+    }, 1000);
+}
+
+countdown(5); // Start a countdown from 5 seconds
