0d80df482c4ae992f4e75f970c1454bedbdb0a40
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-28 12:59:36 +0430

    my commit

---
+++ Quote Generator.js
@@ -0,0 +1,6 @@
+const quotes = ["Quote 1", "Quote 2", "Quote 3"];
+
+function generateQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    document.getElementById('quote').innerText = quotes[randomIndex];
+}
