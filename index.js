9e9d717ef156847732804131b6defaa3868706be
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-28 11:16:54 +0430

    My Initial Commit 3

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ let num1 = 10;
 let num2 = 5;
 let sum = num1 + num2;
 console.log("The sum is: " + sum);
+
+
+
+let x = 10;
+if (x > 5) {
+    console.log("x is greater than 5");
+} else {
+    console.log("x is 5 or less");
+}
