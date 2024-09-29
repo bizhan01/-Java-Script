94faaa6f857ddecf2836618304088fd2edd862f6
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 09:06:02 +0430

    Changes Found

--- Age Calculator.js
+++ Age Calculator.js
@@ -2,5 +2,6 @@ function startTest() {
     // Start the typing test and calculate the typing speed
 }
 
+// javascript tutorial code
 // Author: Rahmatullah Bizhan
 // Email: rahmatullahbizhan@gmail.com
