66352850b2a7e8c53c0511bb5c33798e44ad424d
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 11:44:20 +0430

    Changes Found

--- Simple Currency Converter.js
+++ Simple Currency Converter.js
@@ -2,3 +2,8 @@ const usdAmount = 100;
 const exchangeRate = 0.85;
 const euroAmount = usdAmount * exchangeRate;
 console.log(`$${usdAmount} is €${euroAmount.toFixed(2)}`);
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
