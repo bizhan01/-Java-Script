c406aafc79ae98e96019b5f465b57f255b42a4ba
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 09:17:08 +0430

    Changes Found

--- Quote Generator.js
+++ Quote Generator.js
@@ -4,3 +4,8 @@ function generateQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     document.getElementById('quote').innerText = quotes[randomIndex];
 }
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
