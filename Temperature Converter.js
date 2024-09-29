408739018aa74432ea0952a64019d92f854613a7
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 09:19:20 +0430

    Changes Found

--- Temperature Converter.js
+++ Temperature Converter.js
@@ -3,3 +3,9 @@ function convertTemperature() {
     const fahrenheit = (celsius * 9/5) + 32;
     document.getElementById("fahrenheit").innerText = "Fahrenheit: " + fahrenheit;
 }
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
