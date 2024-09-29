f726eb4eeb7a0f0eb2439e0ad2204d58a15c44d6
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 11:19:24 +0430

    Changes Found

---
+++ Simple Password Generator.js
@@ -0,0 +1,14 @@
+function generatePassword(length) {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    return password;
+}
+
+const passwordLength = 8;
+console.log('Generated Password:', generatePassword(passwordLength));
