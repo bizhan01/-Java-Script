c3021315d34e5b909566c4f9392a76a317cdb2ac
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 11:42:57 +0430

    Changes Found

--- Guessing Game.js
+++ Guessing Game.js
@@ -2,3 +2,12 @@
 // javascript tutorial code
 // Author: Rahmatullah Bizhan
 // Email: rahmatullahbizhan@gmail.com
+
+const secretNumber = Math.floor(Math.random() * 10) + 1;
+let guess;
+
+do {
+    guess = parseInt(prompt('Guess a number between 1 and 10:'));
+} while (guess !== secretNumber);
+
+console.log('Congratulations! You guessed the correct number:', secretNumber);
