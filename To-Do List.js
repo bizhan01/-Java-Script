dc46772ba8bcb66ced085227e1d1780cc8186f7e
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 11:45:22 +0430

    Changes Found

--- To-Do List.js
+++ To-Do List.js
@@ -2,3 +2,8 @@ const todos = ['Learn JavaScript', 'Build a project', 'Celebrate success'];
 todos.forEach((todo, index) => {
     console.log(`${index + 1}: ${todo}`);
 });
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
