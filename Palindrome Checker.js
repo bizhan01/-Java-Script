9248dcba607028c51b2be869a26ac6386c5aeb9e
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 11:43:21 +0430

    Changes Found

--- Palindrome Checker.js
+++ Palindrome Checker.js
@@ -2,3 +2,8 @@ const word = 'racecar';
 const reversedWord = word.split('').reverse().join('');
 const isPalindrome = word === reversedWord;
 console.log(`Is "${word}" a palindrome? ${isPalindrome}`);
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
