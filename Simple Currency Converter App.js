d5456e6dd79bc030cb76bf469d1458f5c507ac8b
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 09:18:45 +0430

    Changes Found

--- Simple Currency Converter App.js
+++ Simple Currency Converter App.js
@@ -5,3 +5,9 @@ function convertCurrency(amount, exchangeRate) {
 const amountInUSD = 100;
 const exchangeRate = 0.85; // EUR to USD
 console.log(`Equivalent amount in EUR: ${convertCurrency(amountInUSD, exchangeRate)}`);
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
