e4218fc0b6c518e9fd9d8e76e872a293ef36b8af
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 09:18:14 +0430

    Changes Found

--- Scissors Game.js
+++ Scissors Game.js
@@ -4,3 +4,8 @@ function playGame(userChoice) {
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     // Determine the winner and display the result on the GUI
 }
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
