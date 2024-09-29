a2625ecfa004573a2c5b25ac417e37da75e2c284
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 09:19:30 +0430

    Changes Found

--- Tip Calculator App.js
+++ Tip Calculator App.js
@@ -5,3 +5,9 @@ function calculateTip(amount, tipPercentage) {
 const billAmount = 50;
 const tipPercentage = 15;
 console.log(`Tip amount: $${calculateTip(billAmount, tipPercentage)}`);
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
