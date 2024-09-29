890d02331b4085499b6960ce134d93472365616b
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 09:19:41 +0430

    Changes Found

--- Todo List GUI.js
+++ Todo List GUI.js
@@ -2,3 +2,9 @@ document.getElementById('addBtn').addEventListener('click', function() {
     const task = document.getElementById('taskInput').value;
     // Add task to the todo list on the GUI
 });
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
