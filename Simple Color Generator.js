648347cafd5c2d14d6e7031404e9a13d77233cd1
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 11:17:31 +0430

    Changes Found

---
+++ Simple Color Generator.js
@@ -0,0 +1,6 @@
+function generateRandomColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor;
+}
+
+console.log('Random Color:', generateRandomColor());
