43ae9515d402044fc5e456b7a3d209765e059802
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 11:43:12 +0430

    Changes Found

--- Palindrome Checker (Function).js
+++ Palindrome Checker (Function).js
@@ -5,3 +5,8 @@ function isPalindrome(word) {
 
 const inputWord = 'level';
 console.log(`Is "${inputWord}" a palindrome? ${isPalindrome(inputWord)}`);
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
